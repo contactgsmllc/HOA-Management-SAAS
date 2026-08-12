c3538f68a47095ef9dd73bc282e04a0b24af94f4
contactgsmllc <contactgsmllc@gmail.com>
2026-08-11 21:43:30 -0400

    Update SecurityConfig.java
    Srini change - .requestMatchers(OPTIONS, "/**").permitAll()

--- backend/src/main/java/com/gstech/saas/platform/security/SecurityConfig.java
+++ backend/src/main/java/com/gstech/saas/platform/security/SecurityConfig.java
@@ -21,6 +21,8 @@ import com.gstech.saas.platform.tenant.multitenancy.TenantFilter;
 
 import lombok.RequiredArgsConstructor;
 
+import static org.springframework.http.HttpMethod.OPTIONS;
+
 @RequiredArgsConstructor
 @Configuration
 @EnableMethodSecurity
@@ -41,6 +43,7 @@ public class SecurityConfig {
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         // Public
+                        .requestMatchers(OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/users/register",
                                 "/users/login",
