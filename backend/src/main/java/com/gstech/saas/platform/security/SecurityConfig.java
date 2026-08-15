b2bad7e334c9afa78d97d3268e5ac1ca96818bac
contactgsmllc <contactgsmllc@gmail.com>
2026-08-15 19:14:44 -0400

    Update SecurityConfig.java
    Srini added https://hoa-app.tailfc43ba.ts.net/

--- backend/src/main/java/com/gstech/saas/platform/security/SecurityConfig.java
+++ backend/src/main/java/com/gstech/saas/platform/security/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         // config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
-        config.setAllowedOrigins(List.of("https://gsittech.com","http://localhost:5173", "http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://hoa-app.tailfc43ba.ts.net/","https://gsittech.com","http://localhost:5173", "http://localhost:3000"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
