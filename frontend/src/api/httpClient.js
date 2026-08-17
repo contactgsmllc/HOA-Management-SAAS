66f4217723ed03d87ce253e3780244bae83bd888
contactgsmllc <contactgsmllc@gmail.com>
2026-08-17 08:54:16 -0400

    Update httpClient.js
    Srini commit

--- frontend/src/api/httpClient.js
+++ frontend/src/api/httpClient.js
@@ -2,8 +2,12 @@
 import axios from "axios";
 import { getAccessToken, setToken, clearStorage } from "../shared/utils/storage";
 
+const API_BASE_URL = import.meta.env.DEV
+  ? import.meta.env.VITE_API_BASE_URL
+  : window.location.origin;
+
 const httpClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: VITE_API_BASE_URL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true, 
 });
