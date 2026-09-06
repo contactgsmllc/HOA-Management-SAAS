af2953dbc89e12eb588ea13ca4c5994af53cdd2b
Saurabh240 <saurabh58833@gmail.com>
2026-09-06 17:38:54 +0530

    updated the fallback url

--- frontend/src/api/httpClient.js
+++ frontend/src/api/httpClient.js
@@ -2,9 +2,7 @@
 import axios from "axios";
 import { getAccessToken, setToken, clearStorage } from "../shared/utils/storage";
 
-const API_BASE_URL = import.meta.env.DEV
-  ? import.meta.env.VITE_API_BASE_URL
-  : window.location.origin;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || window.location.origin;
 
 const httpClient = axios.create({
   baseURL: API_BASE_URL,
